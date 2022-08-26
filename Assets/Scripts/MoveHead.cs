46d85aba5e70655b50388eb5e72c3496a2602c74
xYumix <cool-morze2014@ya.ru>
2022-08-26 19:09:01 +0400

    add game object

---
+++ Assets/Scripts/MoveHead.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+
+public class MoveHead : MonoBehaviour
+{
+    public float forwardSpeed = 10f;
+    public int tailLength = 1;
+    private TailMove TailScript;
+    float screenDistance;
+    Vector3 mousePos;
+    Vector3 screenTouch;
+    bool mouseMoved = false;
+    bool screenTouched = false;
+    float changePos=0;
+
+    float damp = 0.01f;
+
+
+    bool SnakeStopped = false;
+    float deltaTimer = 0;
+
+    public bool TouchedLeft = false;
+    public bool TouchedRight = false;
+    Vector3 StartPos = Vector3.zero;
+
+    public GameController GameController;
+    private void Awake()
+    {
+        TailScript = GetComponent<TailMove>();
+    }
+    void Start()
+    {
+        StartPos = transform.position;
+        GetComponent<MeshRenderer>().material.color = Camera.main.GetComponent<SetColorPattern>().SnakeColor;
+    }
+
+    void Update()
+    {
+        screenDistance = Camera.main.WorldToScreenPoint(transform.position).z;
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenDistance));
+        if (Input.GetMouseButtonDown(0))
+        {
+            screenTouch = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenDistance));
+            screenTouched = true;
+        }
+        if (screenTouched)
+        {
+            changePos = Mathf.Abs(mousePos.x - screenTouch.x);
+            if (changePos > 1f)
+            {
+                mouseMoved = true;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseMoved = false;
+            screenTouched = false;
+        }
+        if (Input.GetMouseButton(0) && mouseMoved && !GameController.GamePaused)
+        {
+
+            Vector3 direction = new Vector3(mousePos.x, transform.position.y, transform.position.z);
+            if (TouchedLeft)
+            {
+                if (direction.x > 0)
+                {
+                    direction.x = 0;
+                }
+            }
+            if (TouchedRight)
+            {
+                if (direction.x < 0)
+                {
+                    direction.x = 0;
+                }
+            }
+            if (!(transform.position.x < StartPos.x - 7 && direction.x < 0) && !(transform.position.x > StartPos.x + 7 && direction.x > 0))
+            {
+                transform.position = Vector3.Lerp(transform.position, direction, damp);
+            }
+        }
+
+        if (!SnakeStopped)
+        {
+            transform.position += Vector3.forward * Time.deltaTime * forwardSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            tailLength++;
+            TailScript.AddCircle();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            tailLength--;
+            TailScript.RemoveCircle();
+        }
+        deltaTimer -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bonus"))
+        {
+            int bonusCount = other.gameObject.GetComponent<Bonus>().bonusCount;
+            tailLength += bonusCount;
+            for(int i = 0; i < bonusCount; i++)
+            {
+                TailScript.AddCircle();
+            }
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("CrashLeft"))
+        {
+            TouchedLeft = true;
+        }
+        if (other.gameObject.CompareTag("CrashRight"))
+        {
+            TouchedRight = true;
+        }
+        if (other.gameObject.CompareTag("EndTrigger"))
+        {
+            GameController.LevelCompleted();
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("CrashBox"))
+        {
+            var crashBox = other.gameObject.GetComponent<Box>();
+            SnakeStopped = true;
+            int crashCount = crashBox.crashCount;
+
+            if (tailLength > 0)
+            {
+                if (deltaTimer < 0 && crashCount > 0)
+                {
+                    crashCount = crashBox.crashCount -= 1;
+                    crashBox.ColorChange();
+                    TailScript.RemoveCircle();
+                    tailLength--;
+                    deltaTimer = 0.05f;
+                }
+                else if (crashCount < 1)
+                {
+                    other.gameObject.SetActive(false);
+                    other.gameObject.GetComponent<Box>().ResetCrashCount();
+                    SnakeStopped = false;
+                }
+            }
+            else
+            {
+                GameController.SnakeCrashed();
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("CrashBox"))
+        {
+            SnakeStopped = false;
+        }
+        if (other.gameObject.CompareTag("CrashLeft"))
+        {
+            TouchedLeft = false;
+        }
+        if (other.gameObject.CompareTag("CrashRight"))
+        {
+            TouchedRight = false;
+        }
+    }
+
+    public void StartReturn()
+    {
+        TailScript.ClearTail();
+        tailLength = 5;
+        transform.position = new Vector3(0, transform.position.y, 0);
+        for (int i = 0; i < tailLength; i++) TailScript.AddCircle();
+        SnakeStopped = false;
+    }
+}
---
+++ Assets/Scripts/TailMove.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TailMove : MonoBehaviour
+{
+    GameObject SnakeHead;
+    public GameObject TailPrefab;
+    float TailElementSize = 0;
+    List<GameObject> Tail = new List<GameObject>();
+    List<Vector3> positions = new List<Vector3>();
+    float headSpeed = 10f;
+
+    int TailCount = 0;
+    private TextMeshProUGUI TailCountText;
+    void Start()
+    {
+        ClearTail();
+        SnakeHead = gameObject;
+        TailElementSize = 0.5f;
+        headSpeed = SnakeHead.GetComponent<MoveHead>().forwardSpeed;
+        TailCountText = transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if(Tail.Count > 0)
+        {
+            UpdateTail();
+        }
+        TailCountText.text = TailCount.ToString();
+    }
+
+
+    void UpdateTail()
+    {
+        float distance = ((Vector3)SnakeHead.transform.position - Tail[0].transform.position).magnitude;
+        
+        if (distance > TailElementSize)
+        {   positions.Clear();
+            positions.Add(SnakeHead.transform.position);
+            foreach (GameObject element in Tail)
+            {
+                positions.Add(element.transform.position);
+            }
+
+            for (int i = 0; i < Tail.Count; i++)
+            {
+                    if (Tail[i].GetComponent<TouchingCrash>().TouchedLeft || Tail[i].GetComponent<TouchingCrash>().TouchedRight)
+                    {
+                        Tail[i].transform.position += Vector3.forward * Time.deltaTime * headSpeed;
+                    }
+                    else
+                    {
+                        Tail[i].transform.position = Vector3.Lerp(positions[i + 1], positions[i], distance / TailElementSize * Time.deltaTime * (headSpeed + 5f));
+                    }
+            }
+           
+        }
+        
+    }
+
+    public void AddCircle()
+    {
+        GameObject TailElement;
+        if (Tail.Count > 0)
+        {
+            TailElement = Instantiate(TailPrefab, Tail[Tail.Count-1].transform.position, Quaternion.identity);
+        }
+        else
+        {
+            TailElement = Instantiate(TailPrefab, SnakeHead.transform.position, Quaternion.identity);
+        }
+        Tail.Add(TailElement);
+        TailElement.GetComponent<MeshRenderer>().material.color = GetComponent<MeshRenderer>().material.color;
+        TailCount++;
+    }
+
+    public void RemoveCircle()
+    {
+        Tail[Tail.Count - 1].GetComponent<TouchingCrash>().Destruction = true;
+        Tail.RemoveAt(Tail.Count - 1);
+        TailCount--;
+    }
+
+    public void ClearTail()
+    {
+        if(Tail.Count > 0)
+        {
+            foreach (GameObject TailElement in Tail)
+            {
+                Destroy(TailElement.gameObject);
+            }
+        }
+        TailCount = 0;
+        Tail.Clear();
+        positions.Clear();
+    }
+}
